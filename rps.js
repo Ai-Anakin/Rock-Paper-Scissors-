e710573dfeadc1c8b8a28a34474fc4148f96bfe5
Anakin-Ai <thealex32232@gmail.com>
2025-06-27 21:23:56 +0300

    Add rps with main logic

---
+++ rps.js
@@ -0,0 +1,62 @@
+const rules = {
+    paper: 'rock',
+    rock: 'scissors',
+    scissors: 'paper'
+};
+
+function getComputerChoice() {
+    const choice = ['rock', 'paper', 'scissors']
+    return choice[Math.floor(Math.random() * 3)]
+    }
+     
+function getHumanChoice () {
+    let choice;
+    do {
+        const input = prompt("Rock, Paper, Scissors?");
+        if (input === null) {
+            return null;
+        }
+        choice = input.toLowerCase();
+    }while (!['rock','paper','scissors'].includes(choice));
+    return choice;
+}
+function playRound(computerChoice, humanChoice) {
+    if (rules[humanChoice] === computerChoice) return "You Win";
+    if (rules[computerChoice] === humanChoice) return "You Lose";
+    return "Draw!";}
+
+
+function playGame() {
+    let currentR = 1;
+    let humanscore = 0;
+    let computerscore = 0;
+    while(humanscore < 3 && computerscore < 3){
+        let computerChoice = getComputerChoice();
+        let humanChoice = getHumanChoice();
+        let result = playRound(computerChoice,humanChoice);
+        if (humanChoice === null) {
+            console.log("Game cancelled")
+            return;
+        }
+        if (result === "You Win") {
+            humanscore++;
+            console.log(`Round ${currentR}`)
+            console.log(`You Win, your score ${humanscore}`)
+            currentR++
+        }else if (result === "You Lose"){
+            computerscore++;
+            console.log(`Round ${currentR}`)
+            console.log(`You lose, computer score ${computerscore}`)
+            currentR++
+        }else {
+            console.log("It's a tie")
+        }
+        }
+        if(computerscore === 3){
+            console.log(`Game is end, Computer Wins, Score: Computer ${computerscore}, Your score ${humanscore}`)
+        }else {
+            console.log(`Game is end, You Win, Score: Your score ${humanscore}, Computer ${computerscore}`)
+        }
+    }
+
+playGame();
