7d8a08126863aebad659e1e5ae9ca10e0893a801
Anakin-Ai <thealex32232@gmail.com>
2025-07-02 20:41:43 +0300

    added images, and simple ui.

--- rps.js
+++ rps.js
@@ -4,9 +4,30 @@ const rules = {
     scissors: 'paper'
 };
 
+const imgChoices = ['rock', 'paper', 'scissors'];
+const humanChoice = document.querySelector('.human-choice img');
+let indexImg = 0;
+
+function updateImgChoice() {
+    humanChoice.src = `${imgChoices[indexImg]}.png` ;
+    humanChoice.alt = imgChoices[indexImg]; 
+}
+const changeButton = document.querySelector('.change-button');
+
+changeButton.addEventListener('click', () => {
+    indexImg = (indexImg + 1 + imgChoices.length) % imgChoices.length;
+    updateImgChoice();
+})
+
+const playRoundButton= document.querySelector('.play-round');
+
+playRoundButton.addEventListener('click', function(){
+    playGame();
+})
+
 function getComputerChoice() {
-    const choice = ['rock', 'paper', 'scissors']
-    return choice[Math.floor(Math.random() * 3)]
+    const choice = ['rock', 'paper', 'scissors'];
+    return choice[Math.floor(Math.random() * 3)];
     }
      
 function getHumanChoice () {
@@ -58,5 +79,3 @@ function playGame() {
             console.log(`Game is end, You Win, Score: Your score ${humanscore}, Computer ${computerscore}`)
         }
     }
-
-playGame();
